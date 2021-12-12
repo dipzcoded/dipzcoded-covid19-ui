1f3ef533a7342962da258b77b8ee4aaa171cc03a
dipzcoded <dipoakerele@gmail.com>
2021-12-12 01:53:48 +0100

    done!

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const menuBtn = document.querySelector(".menu");
+const cancelBtn = document.querySelector(".cancel-menu");
+const mainNav = document.querySelector(".main-nav");
 const navList = document.querySelector(".nav-list");
+const listItems = document.querySelectorAll(".nav-items");
 const toggleMenu = () => {
   navList.classList.toggle("show");
+
+  listItems.forEach((el, index) => {
+    if (!el.style.animation) {
+      el.style.animation = `slideY 0.5s ease forwards ${index / 7 + 1}s `;
+    } else {
+      el.style.animation = "";
+    }
+    menuBtn.classList.toggle("notShowMenu");
+    cancelBtn.classList.toggle("notShowMenu");
+  });
 };
 
-menuBtn.addEventListener("click", toggleMenu);
+const arrayBtn = [menuBtn, cancelBtn];
+const windowEvent = ["scroll"];
+arrayBtn.forEach((el) => el.addEventListener("click", toggleMenu));
+
+listItems.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (Number(window.innerWidth) < 860) {
+      toggleMenu();
+    }
+  });
+});
+
+windowEvent.forEach((winEvt) => {
+  window.addEventListener(winEvt, () => {
+    mainNav.classList.toggle("sticky", window.scrollY > 0);
+  });
+});
